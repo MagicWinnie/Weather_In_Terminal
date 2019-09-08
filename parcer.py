c74f61907d6f8c1b8c0d5d67800dc28ddb1cba99
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2019-09-08 12:40:33 +0700

    optimization

--- parcer.py
+++ parcer.py
@@ -1,97 +1,81 @@
 # -*- coding: utf-8 -*-
-import requests
-from lxml import etree
+import time
+import os
 import itertools
+from coolterm import color
+
+import numpy as np
+from numpy import savetxt
+import pandas as pd
+
+import requests
 from bs4 import BeautifulSoup
 from lxml import html
-from numpy import savetxt
-import numpy as np
-import pandas as pd
-import time
-from coolterm import color
+from lxml import etree
+
 import geocoder
-f = open("weather.txt","r+")
+
+if "weather.txt" in os.listdir("/home/dmitrii/"):
+	f = open("weather.txt","r+", encoding="utf-8")
+else:
+	f = open("weather.txt","w", encoding="utf-8")
+	f.close()
+	f = open("weather.txt","r+", encoding="utf-8")
+
+f.truncate(0)
+
 temp = f.readline()
-try:
-	if temp=="" or time.time()-float(temp)>=3600:
-		res = []
-		geolocation = False
-		if geolocation == True:
-			g = geocoder.ip('me')
-			city = g.city
-			print(city)
-		else:
-			city = 'Novosibirsk'
-		DATAURL = 'https://yandex.ru/pogoda/{0}'.format(city)
-		session = requests.session()
-		req_headers = {
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-			'Accept-Encoding': 'gzip, deflate, br',
-			'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8'
-		}
-		r = session.get(DATAURL, headers=req_headers, allow_redirects=True)
-		html=r.text
-		soup = BeautifulSoup(html,features="lxml")
-		w_list = soup.find_all('div', class_='fact__temp-wrap')[0]
-		humidity_list = soup.find_all('div', class_='fact__props fact__props_position_middle')[0]
-		hourly_list = soup.find_all('div', class_='swiper-wrapper')[0]
-		# print(w_list,humidity_list,hourly_list)
-		first = ""
-		first += color.Cyan+color.Bold+"Weather in "+city+":"+color.ResetAll+"\n"
-		first += color.Bold+"Temperature: "+color.ResetAll
-		temperature = w_list.find_all('span',class_='temp__value')[0].text.strip()
-		first += temperature+"°C"+"\n"
-		condition = w_list.find_all('div', class_='link__condition day-anchor i-bem')[0].text.strip()
-		first += color.Bold+"Condition: "+color.ResetAll+condition+"\n"
-		wind = humidity_list.find_all('dd', class_='term__value')[0].text.strip()
-		first += color.Bold+"Wind: "+color.ResetAll + wind + "\n"
-		humidity = humidity_list.find_all('dl', class_='term term_orient_v fact__humidity')[0].text.strip()
-		first += color.Bold+"Humidity: "+color.ResetAll+humidity+"\n"
-		pressure = humidity_list.find_all('dl', class_='term term_orient_v fact__pressure')[0].find('dd', class_="term__value").text.strip()
-		first += color.Bold+"Pressure: "+color.ResetAll+pressure
-		f.truncate(0)
-		f.write(str(time.time())+'\n')
-		f.write(first)
-		print(first)
-	else:
-		print(f.read())
-except ValueError:
-		res = []
-		geolocation = False
-		if geolocation == True:
-			g = geocoder.ip('me')
-			city = g.city
-			print(city)
-		else:
-			city = 'Novosibirsk'
-		DATAURL = 'https://yandex.ru/pogoda/{0}'.format(city)
-		session = requests.session()
-		req_headers = {
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-			'Accept-Encoding': 'gzip, deflate, br',
-			'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8'
-		}
-		r = session.get(DATAURL, headers=req_headers, allow_redirects=True)
-		html=r.text
-		soup = BeautifulSoup(html,features="lxml")
-		w_list = soup.find_all('div', class_='fact__temp-wrap')[0]
-		humidity_list = soup.find_all('div', class_='fact__props fact__props_position_middle')[0]
-		hourly_list = soup.find_all('div', class_='swiper-wrapper')[0]
-		# print(w_list,humidity_list,hourly_list)
-		first = ""
-		first += color.Cyan+color.Bold+"Weather in "+city+":"+color.ResetAll+"\n"
-		first += color.Bold+"Temperature: "+color.ResetAll
-		temperature = w_list.find_all('span',class_='temp__value')[0].text.strip()
-		first += temperature+"°C"+"\n"
-		condition = w_list.find_all('div', class_='link__condition day-anchor i-bem')[0].text.strip()
-		first += color.Bold+"Condition: "+color.ResetAll+condition+"\n"
-		wind = humidity_list.find_all('dd', class_='term__value')[0].text.strip()
-		first += color.Bold+"Wind: "+color.ResetAll + wind + "\n"
-		humidity = humidity_list.find_all('dl', class_='term term_orient_v fact__humidity')[0].text.strip()
-		first += color.Bold+"Humidity: "+color.ResetAll+humidity+"\n"
-		pressure = humidity_list.find_all('dl', class_='term term_orient_v fact__pressure')[0].find('dd', class_="term__value").text.strip()
-		first += color.Bold+"Pressure: "+color.ResetAll+pressure
-		f.truncate(0)
-		f.write(str(time.time())+'\n')
-		f.write(first)
-		print(first)
+geolocation = False
+
+if geolocation == True:
+	g = geocoder.ip('me')
+	city = g.city
+	print(city)
+else:
+	city = 'Novosibirsk'
+DATAURL = 'https://yandex.com/pogoda/{0}'.format(city)
+
+res = []
+first = ""
+session = requests.session()
+req_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8'
+}
+
+if temp=="" or time.time()-float(temp)>=3600:
+	r = session.get(DATAURL, headers=req_headers, allow_redirects=True)
+	html=r.text
+	soup = BeautifulSoup(html,features="lxml")
+
+	w_list = soup.find_all('div', class_='fact__temp-wrap')[0]
+	humidity_list = soup.find_all('div', class_='fact__props fact__props_position_middle')[0]
+	hourly_list = soup.find_all('div', class_='swiper-wrapper')[0]
+	
+	first += color.Cyan+color.Bold+"Weather in "+city+":"+color.ResetAll+"\n"
+	
+	first += color.Bold+"Temperature: "+color.ResetAll
+	temperature = w_list.find_all('span',class_='temp__value')[0].text.strip()
+	first += temperature+"°C"+"\n"
+	
+	condition = w_list.find_all('div', class_='link__condition day-anchor i-bem')[0].text.strip()
+	first += color.Bold+"Condition: "+color.ResetAll+condition+"\n"
+	
+	wind = humidity_list.find_all('dd', class_='term__value')[0].text.strip()
+
+	first += color.Bold+"Wind: "+color.ResetAll + wind + "\n"
+	
+	humidity = humidity_list.find_all('dl', class_='term term_orient_v fact__humidity')[0].text.strip()
+	first += color.Bold+"Humidity: "+color.ResetAll+humidity+"\n"
+	
+	pressure = humidity_list.find_all('dl', class_='term term_orient_v fact__pressure')[0].find('dd', class_="term__value").text.strip()
+	first += color.Bold+"Pressure: "+color.ResetAll+pressure
+	
+	f.truncate(0)
+	f.write(str(time.time())+'\n')
+	f.write(first)
+	
+	print(first)
+else:
+	print(f.read())
